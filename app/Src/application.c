29a68968f8ef2d4a395a062c8a1ba6967108b2ec
antoniogiacomelli <62488903+antoniogiacomelli@users.noreply.github.com>
2025-03-28 18:36:28 -0300

    VER040

--- app/Src/application.c
+++ app/Src/application.c
@@ -1,12 +1,16 @@
 #include "application.h"
 
-INT stack1[STACKSIZE];
-INT stack2[STACKSIZE];
-INT stack3[STACKSIZE];
+/* make sure stacks are declared as extern in  application.h */
+
+/* keep stacks double-word aligned for ARMv7M */
+INT stack1[STACKSIZE] __attribute__((aligned(8))); 
+INT stack2[STACKSIZE] __attribute__((aligned(8)));
+INT stack3[STACKSIZE] __attribute__((aligned(8)));
 
 
 VOID kApplicationInit(VOID)
 {
+	/* kernel objects are initialised here */
 }
 
 VOID Task3(VOID)
