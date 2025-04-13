11f1d3ad5062d21006b10a3e3f8a25d7e57fd3b3
antoniogiacomelli <antoniogiacomelli@protonmail.com>
2025-04-13 02:23:46 -0300

    Fix: re-add armv6m as a regular folder (no longer submodule)

---
+++ arch/armv6m/Core/app/Inc/application.h
@@ -0,0 +1,35 @@
+#ifndef APPLICATION_H
+#define APPLICATION_H
+#include "kapi.h"
+
+#define RK_NUF030R8
+
+/*******************************************************************************
+ * TASK STACKS EXTERN DECLARATION
+ *******************************************************************************/
+#define STACKSIZE 256 /*you can define each stack with a specific
+ size*/
+
+extern INT stack1[STACKSIZE];
+extern INT stack2[STACKSIZE];
+extern INT stack3[STACKSIZE];
+extern INT stack4[STACKSIZE];
+
+extern RK_TASK_HANDLE task1Handle;
+extern RK_TASK_HANDLE task2Handle;
+extern RK_TASK_HANDLE task3Handle;
+extern RK_TASK_HANDLE task4Handle;
+
+/*******************************************************************************
+ * TASKS ENTRY POINT PROTOTYPES
+ *******************************************************************************/
+
+
+VOID Task1( VOID*);
+VOID Task2( VOID*);
+VOID Task3( VOID*);
+VOID Task4( VOID*);
+
+
+
+#endif /* APPLICATION_H */
