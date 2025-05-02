f0cab0ec953a0080b29523b56bdf5d2d3f92cd68
antoniogiacomelli <antoniogiacomelli@protonmail.com>
2025-05-02 16:37:23 -0300

    independent app

--- app/inc/application.h
+++ app/inc/application.h
@@ -1,19 +1,21 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
+#include <kservices.h>
 #include <kapi.h>
-
-
 /*******************************************************************************
  * TASK STACKS EXTERN DECLARATION
  *******************************************************************************/
-#define STACKSIZE 128 
+#define STACKSIZE 256 /*you can define each stack with a specific size */
+/* this value is in WORDS - make it a multiple of 8 */
+
 extern INT stack1[STACKSIZE];
 extern INT stack2[STACKSIZE];
 extern INT stack3[STACKSIZE];
-
+ 
 extern RK_TASK_HANDLE task1Handle;
 extern RK_TASK_HANDLE task2Handle;
 extern RK_TASK_HANDLE task3Handle;
+ 
 
 /*******************************************************************************
  * TASKS ENTRY POINT PROTOTYPES
@@ -23,7 +25,7 @@ extern RK_TASK_HANDLE task3Handle;
 VOID Task1( VOID*);
 VOID Task2( VOID*);
 VOID Task3( VOID*);
-
+ 
 
 
 #endif /* APPLICATION_H */
