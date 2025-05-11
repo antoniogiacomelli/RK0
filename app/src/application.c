e015b26fd1e94ea8416386b23b388dd5f1d06f6f
antoniogiacomelli <antoniogiacomelli@protonmail.com>
2025-05-11 18:05:21 -0300

    Several prunnings for readability.

---
+++ app/src/application.c
@@ -0,0 +1,137 @@
+#include <application.h>
+#include <stdio.h>
+
+/********** STELLARIS BOARD UART **********/
+
+#ifndef UART0_BASE
+#define UART0_BASE 0x4000C000
+#define UART0_DR  (*(volatile unsigned *)(UART0_BASE + 0x00)) /* Data register */
+#define UART0_FR  (*(volatile unsigned *)(UART0_BASE + 0x18)) /* Fifo register */
+#define UART0_FR_TXFF (1U << 5)   /* FIFO Full */
+#endif
+ 
+static inline VOID kPutc(CHAR const c) 
+{
+	while (UART0_FR & UART0_FR_TXFF)
+        ;
+    UART0_DR = c;
+}
+
+VOID kPuts(const CHAR *str) 
+{
+    while(*str) 
+    {
+        kPutc(*str++);
+    }
+}
+
+/* this is for printf */
+
+#define __io_putchar(ch) \
+do { \
+	kPutc((CHAR const)ch); \
+} while(0U)
+
+/* weakly declared on syscalls.c */
+int _write(int file, char *ptr, int len)
+{
+  (void)file;
+  int idx;
+
+  for (idx = 0; idx < len; idx++)
+  {
+	__io_putchar(*ptr++);
+  }
+  return (len);
+}
+
+/********** APPLICATION: SYNCHRONISATION BARRIER **********/
+
+/* in this example, stacks and task handles are extern declared in application.h that is included in main.c */
+RK_TASK_HANDLE task1Handle;
+RK_TASK_HANDLE task2Handle;
+RK_TASK_HANDLE task3Handle;
+
+/* keep stacks double-word aligned for ARMv6/7M */
+INT stack1[STACKSIZE] __RK_ALIGN(8);
+INT stack2[STACKSIZE] __RK_ALIGN(8);
+INT stack3[STACKSIZE] __RK_ALIGN(8);
+
+
+/* Kernel objects */
+
+RK_EVENT syncEvent;  
+RK_MUTEX syncMutex;  
+
+UINT syncCounter; 
+
+/* Initialise kernel objects - this function definition is mandatory, even if empty */
+VOID kApplicationInit(VOID)
+{
+	kMutexInit(&syncMutex);
+	kEventInit(&syncEvent);
+	syncCounter = 0;
+}
+
+#define SYNC_CONDITION (3) /* needed tasks in the barrier */
+
+static VOID synch(VOID)
+{
+	kMutexLock(&syncMutex, RK_INHERIT, RK_WAIT_FOREVER);
+	syncCounter += 1;
+	if (syncCounter < SYNC_CONDITION)
+	{
+	    /* must be atomic */
+	    kDisableIRQ();
+		kMutexUnlock(&syncMutex);
+		kEventSleep(&syncEvent, RK_WAIT_FOREVER);
+		kEnableIRQ();
+        kMutexLock(&syncMutex, RK_INHERIT, RK_WAIT_FOREVER);
+	}
+	else
+	{  
+        syncCounter = 0;
+		printf("%s wakes all tasks...\n\r", RK_RUNNING_NAME);
+		kEventWake(&syncEvent);
+	
+    }
+	printf("%s synch'd \n\r", RK_RUNNING_NAME);
+    kMutexUnlock(&syncMutex);
+}
+
+
+
+VOID Task1(VOID* args)
+{
+    RK_UNUSEARGS
+	while (1)
+	{
+		kSleep(4);
+        printf("Task 1 is synching...\n\r");
+		kBusyWait(100);
+		synch();
+        
+	}
+}
+VOID Task2(VOID* args)
+{
+    RK_UNUSEARGS
+	while (1)
+	{
+		kSleep(8);
+        printf("Task 2 is synching...\n\r");
+		kBusyWait(100);
+		synch();
+	}
+}
+VOID Task3(VOID* args)
+{
+    RK_UNUSEARGS
+	while (1)
+	{
+		kSleep(4);
+        printf("Task 3 is synching...\n\r");
+		kBusyWait(100);
+		synch();
+	}
+}
