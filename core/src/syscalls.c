25743b164705371a5b11db4032e6c9dcaf721119
antoniogiacomelli <antoniogiacomelli@protonmail.com>
2025-05-13 18:52:46 -0300

    back-end sycalls rework

--- core/src/syscalls.c
+++ core/src/syscalls.c
@@ -8,24 +8,10 @@ extern int errno;
 
 __RK_WEAK
 caddr_t _sbrk(int incr)
-{
-  extern char _heap_start;  
-  extern char _heap_end;      
-
-  static char *heap_curr;    
-    if (heap_curr == 0)
-        heap_curr = &_heap_start;      
-
-    char *prev = heap_curr;
-    char *next = heap_curr + incr;
-
-    if (next < &_heap_start || next > &_heap_end) {
-        errno = ENOMEM;
-        return (caddr_t)-1;
-    }
-
-    heap_curr = next;
-    return (caddr_t)prev;
+{ 
+    (void)incr;
+    errno = ENOMEM;
+    return (caddr_t)-1;
 }
     
 __RK_WEAK
