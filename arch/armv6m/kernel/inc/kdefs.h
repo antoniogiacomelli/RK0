c6a38e5c9ce8782cd2357c692b4573163089ffec
antoniogiacomelli <antoniogiacomelli@protonmail.com>
2025-06-02 23:11:14 -0300

    Minors

--- arch/armv6m/kernel/inc/kdefs.h
+++ arch/armv6m/kernel/inc/kdefs.h
@@ -68,7 +68,7 @@ unsigned kIsISR( void)
 
 /* place table on ram for efficiency */
 __attribute__((section(".tableGetReady"), aligned(4)))
-static unsigned table[32] = 
+const static unsigned table[32] = 
 {
  0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
  31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
