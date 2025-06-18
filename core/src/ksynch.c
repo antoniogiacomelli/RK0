2c83bd9cceeab88b67b5e3c4ea70f991c0bcd01d
antoniogiacomelli <antoniogiacomelli@protonmail.com>
2025-06-17 23:23:28 -0300

    Rever changes on broadcast

--- core/src/ksynch.c
+++ core/src/ksynch.c
@@ -421,8 +421,7 @@ RK_ERR kEventWake(RK_EVENT *const kobj, UINT nTasks, UINT *uTasksPtr)
         RK_TCB *nextTCBPtr = NULL;
         kTCBQDeq(&kobj->waitingQueue, &nextTCBPtr);
         kReadyCtxtSwtch(nextTCBPtr);
-        RK_CR_EXIT
-        RK_CR_ENTER
+
     }
     if (uTasksPtr)
         *uTasksPtr = (UINT)kobj->waitingQueue.size;
@@ -739,8 +738,6 @@ RK_ERR kSemaWake(RK_SEMA *const kobj, UINT const nTasks, UINT *const uTasksPtr)
         RK_TCB *nextTCBPtr = NULL;
         kTCBQDeq(&kobj->waitingQueue, &nextTCBPtr);
         kReadyCtxtSwtch(nextTCBPtr);
-        RK_CR_EXIT
-        RK_CR_ENTER
     }
 
     if (uTasksPtr)
