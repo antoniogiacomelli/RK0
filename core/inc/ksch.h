bbc2668f57e36875bccc2586fddb13592a0e43bd
antoniogiacomelli <antoniogiacomelli@protonmail.com>
2025-07-06 12:57:50 -0300

    V0.6.5

--- core/inc/ksch.h
+++ core/inc/ksch.h
@@ -3,7 +3,7 @@
  *
  *                     RK0 — Real-Time Kernel '0'
  *
- * Version          :   V0.6.4
+ * Version          :   V0.6.5
  * Architecture     :   ARMv6/7m
  *
  * Copyright (C) 2025 Antonio Giacomelli
