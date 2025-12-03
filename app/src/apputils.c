47df265ec39e9f1eb37bd1c13545a44b814ab24d
antoniogiacomelli <antoniogiacomelli@protonmail.com>
2025-12-03 00:00:44 -0300

    Minors

--- app/src/apputils.c
+++ app/src/apputils.c
@@ -6,50 +6,49 @@ on the _write backend syscall so printf can be used. Note this implementation
 is assuming the QEMU machine (Texas stellaris cortex m3) register map.
 */
 
-
 #if (QEMU_MACHINE == lm3s6965evb)
 
-void kPutc(char const c) 
+void kPutc(char const c)
 {
-	while (UART0_FR & UART0_FR_TXFF)
+    while (UART0_FR & UART0_FR_TXFF)
         ;
     UART0_DR = c;
 }
 
-void kPuts(const char *str) 
+void kPuts(const char *str)
 {
-    while(*str) 
+    while (*str)
     {
         kPutc(*str++);
     }
-    
- }
- 
- /* !! NOTE THE IRQ DISABLE IN _WRITE !! */
+}
 
- int _write(int file, char const *ptr, int len)
+/* !! NOTE THE IRQ DISABLE IN _WRITE !! */
+
+int _write(int file, char const *ptr, int len)
 {
-  __disable_irq();
-  (void)file;
-  int DataIdx;
+    RK_CR_AREA
+    RK_CR_ENTER
+    (void)file;
+    int DataIdx;
 
-  for (DataIdx = 0; DataIdx < len; DataIdx++)
-  {
-    kPutc(*ptr++);
-  }
-  __enable_irq();
-  return len;
+    for (DataIdx = 0; DataIdx < len; DataIdx++)
+    {
+        kPutc(*ptr++);
+    }
+    RK_CR_EXIT
+    return (len);
 }
 #else
-void kPutc(char const c) 
+void kPutc(char const c)
 {
-	(void)c;
-	return;
+    (void)c;
+    return;
 }
 
-void kPuts(const char *str) 
+void kPuts(const char *str)
 {
-	(void)str;
-	return;
+    (void)str;
+    return;
 }
 #endif
