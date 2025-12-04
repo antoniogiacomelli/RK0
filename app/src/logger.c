04faa54ef53fc0edc91163b7796da3527ea13b1b
antoniogiacomelli <antoniogiacomelli@protonmail.com>
2025-12-04 01:49:19 -0300

    Logger facility refactored

--- app/src/logger.c
+++ app/src/logger.c
@@ -14,12 +14,16 @@
 
 #if (CONF_LOGGER == ON)
 
-#define LOGLEN 64 /* max length of a single log message */
+#define LOGLEN 64    /* max length of a single log message */
 #define LOGPOOLSIZ 8 /* number of buffers in the pool */
 #define LOG_STACKSIZE 128
 
-static UINT logMemErr = 0; /* number of failing allocs (if missing prints) */
+#define LOG_COUNT_ERR 0
+
+#if (LOG_COUNT_ERR == 1)
+static UINT logMemErr = 0;   /* number of failing allocs (if missing prints) */
 static UINT logQueueErr = 0; /* number of failing queue sends (if missing prints) */
+#endif
 struct log
 {
     RK_TICK t;
@@ -53,54 +57,56 @@ VOID kprintf(const char *fmt, ...)
 /* formatted string input */
 VOID logPost(const char *fmt, ...)
 {
+#if (LOG_COUNT_ERR == 1)
+    RK_CR_AREA
+#endif
 
     Log_t *logPtr = kMemPartitionAlloc(&qMem);
     if (logPtr)
     {
- 
-        kSchLock();
         VOID *p = logPtr;
         logPtr->t = kTickGetMs();
+
         va_list args;
         va_start(args, fmt);
-        int len = vsnprintf(logPtr->s, sizeof(logPtr->s), fmt, args);
+        vsnprintf(logPtr->s, sizeof logPtr->s, fmt, args);
         va_end(args);
-        if (len >= (int)sizeof(logPtr->s))
-        {
-            /*if message is larger than LOGLEN trunk with '...' */
-            if (len > 4)
-            {
-                RK_STRCPY(&logPtr->s[len - (int)4], "...");
-            }
-        }
-        kSchUnlock();
+
         if (kMesgQueueSend(&logQ, &p, RK_NO_WAIT) != RK_ERR_SUCCESS)
         {
             kMemPartitionFree(&qMem, p);
+
+#if (LOG_COUNT_ERR == 1)
+            RK_CR_ENTER;
             logQueueErr++;
+            RK_CR_EXIT;
+#endif
         }
-        
     }
     else
     {
+#if (LOG_COUNT_ERR == 1)
+        RK_CR_ENTER;
         logMemErr++;
+        RK_CR_EXIT;
+#endif
     }
-
 }
+
 static VOID LoggerTask(VOID *args)
 {
     RK_UNUSEARGS
     while (1)
     {
 
-            VOID *recvPtr = NULL;
-            while (kMesgQueueRecv(&logQ, &recvPtr, RK_WAIT_FOREVER) == RK_ERR_SUCCESS)
-            {
-                K_ASSERT(recvPtr != NULL);
-                Log_t *logPtr = (Log_t *)recvPtr;
-                kprintf("%8lu ms :: %s \r\n", logPtr->t, logPtr->s);
-                kMemPartitionFree(&qMem, recvPtr);
-            }
+        VOID *recvPtr = NULL;
+        while (kMesgQueueRecv(&logQ, &recvPtr, RK_WAIT_FOREVER) == RK_ERR_SUCCESS)
+        {
+            K_ASSERT(recvPtr != NULL);
+            Log_t *logPtr = (Log_t *)recvPtr;
+            kprintf("%8lu ms :: %s \r\n", logPtr->t, logPtr->s);
+            kMemPartitionFree(&qMem, recvPtr);
+        }
     }
 }
 
@@ -109,8 +115,8 @@ VOID logInit(RK_PRIO priority)
     K_ASSERT(!kMemPartitionInit(&qMem, logBufPool, sizeof(Log_t), LOGPOOLSIZ));
     K_ASSERT(!kMesgQueueInit(&logQ, logQBuf, RK_MESGQ_MESG_SIZE(VOID *), LOGPOOLSIZ));
     K_ASSERT(!kCreateTask(&logTaskHandle, LoggerTask, RK_NO_ARGS,
-                         "LogTsk", logstack, LOG_STACKSIZE,
-                         priority, RK_PREEMPT));
+                          "LogTsk", logstack, LOG_STACKSIZE,
+                          priority, RK_PREEMPT));
 }
 
 #endif
