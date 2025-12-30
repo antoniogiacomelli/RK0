bd57935839d90b8f2d7f534847319bd0e79928e6
Antonio Giacomelli <62488903+antoniogiacomelli@users.noreply.github.com>
2025-12-30 00:53:44 -0300

    Clarify comment on flush sema

--- core/src/ksema.c
+++ core/src/ksema.c
@@ -257,7 +257,7 @@ RK_ERR kSemaphoreFlush(RK_SEMAPHORE *const kobj)
                 chosenTCBPtr = nextTCBPtr;
         }
     }
-    kobj->value = 0U; /* flush resets count to 0 */
+    kobj->value = 0U; /* flushed, count set to 0 */
     kSchedTask(chosenTCBPtr); 
     RK_CR_EXIT
     return (RK_ERR_SUCCESS);
